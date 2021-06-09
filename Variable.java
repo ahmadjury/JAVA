d65a3c5095a7a237d2bc6dee58ae908c1ecf96a4
ahmad jury <ahmadjury12@gmail.com>
2021-06-09 12:03:29 +0300

    First assinment

---
+++ Variable.java
@@ -0,0 +1,10 @@
+public class Variable{
+	public static void main(String[] args){
+		int ourint;
+		ourint = 400;
+		double pi = 3.14159265;
+		boolean bool = true;
+		char singleCharacter = 'A';
+		String multipleCharacters = "ABC";
+	}
+}
